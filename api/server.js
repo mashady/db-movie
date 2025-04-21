947f26949057cb3dea7d17c58bd74d918338df8c
mashady <muhammedmashady@gmail.com>
2025-04-21 11:59:57 +0200

    0

--- api/server.js
+++ api/server.js
@@ -1,31 +1,32 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const path = require('path');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+server.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+server.use((req, res, next) => {
+  const invalidIdMatch = req.url.match(/\/\w+\/(null|undefined)(\/)?$/);
+  if (invalidIdMatch) {
+    return res.status(400).json({ error: 'Invalid ID in URL' });
+  }
+  next();
+});
 
-const middlewares = jsonServer.defaults()
+server.use(middlewares);
 
-server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
+}));
 
-// Export the Server API
-module.exports = server
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running on port 3000');
+});
